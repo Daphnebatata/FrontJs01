8a08b9960867f0f8343ea52f03283361e8f9682b
BatataD <daphne.e.silva@escola.pr.gov.br>
2024-10-01 13:20:19 +0000

    Criação da operação de adição

--- src/index.mjs
+++ src/index.mjs
@@ -1,1 +1,22 @@
-//Inicio do projerto calculadora HTML/CSS/JS
+//consInicio do projerto calculadora HTML/CSS/JS
+const app = document.querySelector("#app");
+const numero1 = document.querySelector("#numero1");
+const numero2 = document.querySelector("#numero2");
+const btnCalcular = document.querySelector("#btn-calcular");
+const resultados = document.querySelector("#resultados");
+const soma = document.querySelector(".soma");
+const subtracao = document.querySelector(".subtracao");
+const multiplicacao = document.querySelector(".multiplicacao");
+const divisao = document.querySelector(".divisao");
+
+function calcular() {
+  const n1 = Number(numero1.value);
+  const n2 = Number(numero2.value);
+  res = n1 + n2;
+soma.innerHTML = 'A soma de $(n1) e $(n2) é: $(n1+n2)';
+}
+calcular();
+btnCalcular.addEventListener("click", function (evento) {
+  alert("clicou");
+  calcular();
+};
