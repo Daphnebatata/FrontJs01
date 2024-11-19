c4ffdbfdf0a6f1b532c951883a4ab4908f5c4f0c
BatataD <daphne.e.silva@escola.pr.gov.br>
2024-11-19 13:07:09 +0000

    calculadora

--- src/index.mjs
+++ src/index.mjs
@@ -3,7 +3,7 @@ const app = document.querySelector("#app");
 const numero1 = document.querySelector("#numero1");
 const numero2 = document.querySelector("#numero2");
 const btnCalcular = document.querySelector("#btn-calcular");
-const resultados = document.querySelector("#resultados");
+const resultados = document.querySelector(".resultados");
 const soma = document.querySelector(".soma");
 const subtracao = document.querySelector(".subtracao");
 const multiplicacao = document.querySelector(".multiplicacao");
@@ -13,13 +13,14 @@ function calcular() {
   const n1 = Number(numero1.value);
   const n2 = Number(numero2.value);
   res = n1 + n2;
-  resultados.classList.remove("esconder");
-  resultados.classList.add("mostrar");
 
   soma.innerHTML = `A soma de ${n1} e ${n2} é: ${n1 + n2}`;
   subtracao.innerHTML = `A subtração de ${n1} e ${n2} é: ${n1 - n2}`;
   multiplicacao.innerHTML = `A multiplicação de ${n1} e ${n2} é: ${n1 * n2}`;
   divisao.innerHTML = `A divisão de ${n1} e ${n2} é: ${n1 / n2}`;
+
+  resultados.classList.add("mostrar");
+  resultados.classList.remove("esconder");
 }
 
 btnCalcular.addEventListener("click", function (evento) {
